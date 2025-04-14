5331d74548f55606da2416374918a96453918230
LiveisFpv <nasonovnicolai@gmail.com>
2025-04-14 11:06:07 +0300

    prompt

--- test_client.go
+++ test_client.go
@@ -23,10 +23,10 @@ func main() {
 	client := pb.NewSemanticServiceClient(conn)
 
 	// Отправляем запрос
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
 
-	req := &pb.SearchRequest{InputData: "Data"}
+	req := &pb.SearchRequest{InputData: "Machine learning for scientific research"}
 	res, err := client.SearchPaper(ctx, req)
 	if err != nil {
 		log.Fatalf("Ошибка при вызове SearchPaper: %v", err)
